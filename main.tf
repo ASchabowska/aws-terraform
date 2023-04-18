63a785cdc6f1a42ace91acae4863863f6a2ebebb
ASchabowska <agata.schabowska@gmail.com>
2023-04-18 15:56:31 +0200

    added gateways and vpc endpoints

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.63"
     }
   }
 
@@ -21,13 +21,12 @@ resource "aws_vpc" "main" {
     } 
 }
 
-resource "aws_subnet" "public_subnet" {
- vpc_id     = aws_vpc.main.id
- cidr_block = "10.0.1.0/24"
- 
- tags = {
-   Name = "Public Subnet"
- }
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main gateway"
+  }
 }
  
 resource "aws_subnet" "private_subnet" {
@@ -35,6 +34,59 @@ resource "aws_subnet" "private_subnet" {
  cidr_block = "10.0.2.0/24"
  
  tags = {
-   Name = "Private Subnet"
+   Name = "private subnet"
  }
+}
+
+resource "aws_subnet" "public_subnet" {
+ vpc_id     = aws_vpc.main.id
+ cidr_block = "10.0.1.0/24"
+ 
+ tags = {
+   Name = "public subnet"
+ }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc      = true
+  tags = {
+    "Name" = "gateway eip"
+  }
+}
+
+resource "aws_nat_gateway" "public_nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "public nat gateway"
+  }
+  depends_on = [aws_internet_gateway.gateway]
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-west-2.ssm"
+
+  tags = {
+    Environment = "ssm endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "sm" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-west-2.secretsmanager"
+
+  tags = {
+    Environment = "sm endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "event_bridge" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-west-2.eventbridge"
+
+  tags = {
+    Environment = "event bridge endpoint"
+  }
 }
